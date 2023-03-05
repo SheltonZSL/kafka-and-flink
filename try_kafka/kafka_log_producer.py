3543306391ed57ce10dde1decf47a68afff836c1
SheltonZSL <Sheltonzsl123@gmail.com>
2023-03-04 20:29:12 -0500

    change files

--- try_kafka/kafka_log_producer.py
+++ try_kafka/kafka_log_producer.py
@@ -1,8 +1,17 @@
 import time
 
+import confluent_kafka
 from confluent_kafka import Producer
+from confluent_kafka.admin import AdminClient
+from confluent_kafka.cimpl import NewTopic
 
-p = Producer({'bootstrap.servers': 'localhost:9093'})
+# producer with 10 partitions
+p = Producer({'bootstrap.servers': 'localhost:9093',
+              'linger.ms': 100,
+              'acks': 1,
+              'batch.size': 100000,
+              })
+a = AdminClient({'bootstrap.servers': 'localhost:9093'})
 
 
 def delivery_report(err, msg):
@@ -25,18 +34,31 @@ def log_generator():
 
 
 def produce_to_kafka():
-    for data in log_generator():
-        # Trigger any available delivery report callbacks from previous produce() calls
-        p.poll(0)
 
-        # Asynchronously produce a message. The delivery report callback will
-        # be triggered from the call to poll() above, or flush() below, when the
-        # message has been successfully delivered or failed permanently.
-        p.produce('log_topic', data.encode('utf-8'), callback=delivery_report)
-
-    # Wait for any outstanding messages to be delivered and delivery report
-    # callbacks to be triggered.
-    p.flush()
+    # create a new topic with 10 partitions
+    a.create_topics([NewTopic('log_topic', num_partitions=10, replication_factor=1)])
+    counter = 0
+    checkpoint = 0
+    last_counter = 0
+    while 1:
+        time.sleep(0.04)
+        for data in log_generator():
+            # Trigger any available delivery report callbacks from previous produce() calls
+            p.poll(0)
+            #time.sleep(0.001)
+            counter += 1
+            if time.time() - checkpoint >= 1:
+                print("Produced {} messages in 1 seconds".format(counter-last_counter))
+                checkpoint = time.time()
+                last_counter = counter
+            # Asynchronously produce a message. The delivery report callback will
+            # be triggered from the call to poll() above, or flush() below, when the
+            # message has been successfully delivered or failed permanently.
+            p.produce('log_topic', data.encode('utf-8'), partition=counter % 10) #, callback=delivery_report)
+            # produce to multiple partitions
+        # Wait for any outstanding messages to be delivered and delivery report
+        # callbacks to be triggered.
+        p.flush()
 
 
 if __name__ == '__main__':
